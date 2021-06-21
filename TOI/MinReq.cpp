26ab24a7af1aa442c66cebd783bcd5d5f46364bf
Borworntat Dendumrongkul <mastericez890@gmail.com>
2021-06-21 08:15:09 +0700

    Brute Force

--- TOI/MinReq.cpp
+++ TOI/MinReq.cpp
@@ -13,18 +13,17 @@ using namespace std;
 
 using ll = long long;
 
-int l[11], a[11], s[11], t[11], q[11];
+int l[11], a[11], s[11], t[11], q[15];
 int n, m, x;
 
 bool solve2(int i){
 	for(int i=1; i<=n; ++i){
 		cin >> q[i];
 	}
-	vector<int>w;
-	int j = 0;
-	for(int i=0; i<n; ++i){
+	q[n+1] = m + 1;
+	vector<int>nin;
+	for(int i=1; i<=n; ++i){
 		int l, r;
-		l = 0, r = v.size();
 	}
 }
 
