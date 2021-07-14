3448876b358e07afb089c0aa9cdd41e9a58e288f
Borworntat Dendumrongkul <mastericez890@gmail.com>
2021-07-14 09:51:53 +0700

    Dynamite

--- AgileContest/Round_2/AG_Tile.cpp
+++ AgileContest/Round_2/AG_Tile.cpp
@@ -23,13 +23,42 @@ using ll = long long;
 
 int n, m;
 
+char a[3][5050];
+int dp[3][5050];
+
+const int MOD = 1e9 + 7;
+
 void solve(){
 	cin >> n;
-	vector<string> v(n);
-	for(auto &x: v){
-		cin >> x;
+	memset(dp, 0, sizeof dp);
+	for(int i=1; i<=2; ++i){
+		for(int j=1; j<=n; ++j){
+			cin >> a[i][j];
+		}
 	}
-	cout << 4 * n + 4;
+	dp[1][0] = dp[2][0] = 1;
+	for(int i=1; i<=n; ++i){
+		if(a[1][i] == '.' && a[2][i] == '.'){
+			dp[1][i] = dp[1][i-1] + dp[2][i-1];
+			dp[1][i] %= MOD;
+			dp[2][i] = dp[1][i];
+		}
+		else if(a[1][i] == '#' && a[2][i] == '.'){
+			dp[2][i] = dp[2][i-1];
+			dp[2][i] %= MOD;
+			dp[1][i] = 0;
+		}
+		else if(a[1][i] == '.' && a[2][i] == '#'){
+			dp[1][i] = dp[1][i-1];
+			dp[1][i] %= MOD;
+			dp[2][i] = 0;
+		}
+		else{
+			dp[1][i] = dp[0][i] = 0;
+		}
+//		cout << "(" << dp[1][i] << ", " << dp[2][i] << ")" << endl;
+	}
+	cout << (dp[2][n] + dp[1][n])%MOD ; 
 	return ;
 }
 
